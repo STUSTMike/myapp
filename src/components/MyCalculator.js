06d5dd417d3869bf2b0d5d7d63cd085a0fb78cfe
STUSTMike <112836953+STUSTMike@users.noreply.github.com>
2024-05-16 16:26:43 +0800

    Summary

---
+++ src/components/MyCalculator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Calculator.css';
+
+function MyCalculator() {
+    // 在這裡撰寫你的程式碼
+    return (
+        <div className='calculator'> 
+           <h1>我的計算機</h1>
+        </div>
+    );
+}
+
+export default MyCalculator;
